0816d57258bdafe06455def4177a1edd813055f4
Petros <petrospetrosyanjs.96@gmail.com>
2020-08-25 01:46:17 +0400

    Updates

---
+++ precache-manifest.25b57d049e9fdefcc8dfc65db918e4e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eae8240542e5688d02732c2c5d9b3b1",
+    "url": "/todo_list/index.html"
+  },
+  {
+    "revision": "3bb6d71ddaf4e089ecc5",
+    "url": "/todo_list/static/css/main.89710e4b.chunk.css"
+  },
+  {
+    "revision": "abe072170eabcb30b140",
+    "url": "/todo_list/static/js/2.f861f127.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo_list/static/js/2.f861f127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb6d71ddaf4e089ecc5",
+    "url": "/todo_list/static/js/main.22074232.chunk.js"
+  },
+  {
+    "revision": "cf74516e357f9a0d23f8",
+    "url": "/todo_list/static/js/runtime-main.7a3e04cd.js"
+  }
+]);
